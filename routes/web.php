76ce613fe62552601f22b67d553354e5dde025e0
Amritgrground <105211795+Amritgrground@users.noreply.github.com>
2022-05-30 15:47:56 +0100

    database route added

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,26 @@ Route::get('/sendamrit', 'CalendarController@reply');
 Route::get('/calendar', 'CalendarController@calendar');
 Route::get('/messageA', 'CalendarController@amritmail');
 Route::get('/calendar/new', 'CalendarController@getNewEventForm');
+Route::get('/calendar/news', 'CalendarController@getNewEventForms');
+Route::get('/calendar/newsss', 'CalendarController@amritmails');
+Route::get('/calendar/newss', 'CalendarController@databasesget');
 Route::post('/calendar/new', 'CalendarController@createNewEvent');
 Route::get('/calendarA', 'PatroController@patro');
 Route::get('/emailA', 'EmailController@upload');
 Route::post('/eventSend',[
     'uses'=>'fileController@createNewEvent',
     'as'=>'messageSend'
+]);
+
+Route::get('/datas', 'CalendarController@amritmails');
+
+Route::post('/data',[
+    'uses'=>'CalendarController@datas',
+    'as'=>'data'
+]);
+Route::get('/databasesget',[
+    'uses'=>'CalendarController@databases',
+    'as'=>'databases'
 ]);
 Route::post('/messagedownload',[
     'uses'=>'CalendarController@download',
